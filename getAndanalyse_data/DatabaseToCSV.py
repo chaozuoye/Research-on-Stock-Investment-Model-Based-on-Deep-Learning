d0d45977d27eec932b9ff168efc15f829d679345
NEWBEESL <924989973@qq.com>
2021-07-31 18:10:43 +0800

    20210731更新

--- getAndanalyse_data/DatabaseToCSV.py
+++ getAndanalyse_data/DatabaseToCSV.py
@@ -19,7 +19,7 @@ class translate():
         db=sqla.create_engine('mysql+pymysql://root:LH001103@localhost:3306/guba?charset=utf8')
         df=pd.read_sql('select * from info_guba',db)
         print(df)
-        df.to_csv('test.csv',encoding="ansi")   #存为ANSI格式主要是防止打开csv文件乱码
+        df.to_csv('../output/test.csv',encoding="ansi")   #存为ANSI格式主要是防止打开csv文件乱码
 if __name__ == "__main__":
     host,tablename,user,password='127.0.0.1','guba.info_guba','root','LH001103'
     test=translate(host=host,tablename=tablename,user=user,db='guba',password=password)
